4959131825db7295811c7ac961e53b071835defe
Jeff Scott <jpscott@ranken.edu>
2020-03-27 12:03:29 -0500

    Commit on updated RVExample.java file

---
+++ app/src/main/java/com/example/rvexample/MyAdapter.java
@@ -0,0 +1,73 @@
+package com.example.rvexample;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    Context   context;
+
+    String[]  data1;
+    String[]  data2;
+    int[]     images;
+
+    //	Constructor
+    public MyAdapter(Context ct, String[] s1, String[] s2, int[] img) {
+        context = ct;
+        data1 = s1;
+        data2 = s2;
+        images = img;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, final int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        holder.myImageView.setImageResource(images[position]);
+
+        holder.mainLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, SecondActivity.class);
+                intent.putExtra("data1", data1[position]);
+                intent.putExtra("data2", data2[position]);
+                intent.putExtra("myImageView", images[position]);
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView  myText1;
+        TextView  myText2;
+        ImageView myImageView;
+        ConstraintLayout mainLayout;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            myText1     = itemView.findViewById(R.id.title);
+            myText2     = itemView.findViewById(R.id.description);
+            myImageView = itemView.findViewById(R.id.mainImageView);
+            mainLayout  = itemView.findViewById(R.id.mainLayout);
+        }
+    }
+}
